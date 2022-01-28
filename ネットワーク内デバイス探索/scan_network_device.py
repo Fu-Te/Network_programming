c8d2bf9eeedb032f56dc7b94cfb9e0413f93a4c6
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-01-29 00:23:10 +0900

    add ICMP and ARP packets to take opponent IP addr and Mac addr
    IPv4が見つからないエラーで自身のコンピュータではまだ動作確認ができていない．

--- ネットワーク内デバイス探索/scan_network_device.py
+++ ネットワーク内デバイス探索/scan_network_device.py
@@ -5,6 +5,7 @@ import subprocess as sbp
 
 # IPアドレスのネットワーク部を特定する．
 def get_network_part(my_ipaddr):
+    #自身のIPアドレスを取得する方法があれば，それを使った方がユーザからの入力を待たなくていいので，いいかもしれない．　s
     #自身のIPアドレスとサブネットマスクからブロードキャストアドレスを求める．
     #/24,/16に対応するため（ネットワーク部を指定できるように）に要改善．
     count = 0
@@ -31,7 +32,8 @@ def broadcastaddr(network_part):
     # ICMPパケットを流して結果を1つだけ受け取る dstの後をブロードキャストアドレスにしたいところ
     # ネットワーク部だけ入力してもらって，後は繰り返す？ 192.168.1.0~192.168.1.254みたいに．
     #broadcast_ipaddr = 
-    receives = []
+    receives_ICMP = []
+    receives_ARP = []
     for i in range (1,10):
         i = str(i)
         dst_addr = network_part + i
@@ -39,9 +41,17 @@ def broadcastaddr(network_part):
         print('現在探索中のアドレスは{}'.format(dst_addr))
         
         #results = srp1(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op=1,pdst=dst_addr))
-        results = sr1(IP(dst = dst_addr)/ICMP(),timeout = 3)
-        receives.append(results)
-    return results
+        results = sr1(IP(dst = dst_addr)/ICMP(),timeout = 1)
+        receives_ICMP.append(results)
+        
+        results = sr1(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op=1, pdst = dst_addr),timeout = 3)
+        receives_ARP.append(results)
+        
+        
+    
+    print(receives_ARP)
+    print(receives_ICMP)
+    #return results
 
 # arp
 def get_macaddr():
