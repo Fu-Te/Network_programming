a34b64fa712e7e465b21421b1bc5f8f3eb4f557d
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-01-29 23:43:17 +0900

    add

--- Python/ARP.py
+++ Python/ARP.py
@@ -5,6 +5,5 @@ target_ip = input('対象ipアドレス:')
 
 frame = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op=1, pdst = target_ip)
 receive = (srp1(frame))
-receive = bytes.hex(receive)
-#receive.decode('hex')
-print(receive)
+
+print(receive[Ether].src)
--- ネットワーク内デバイス探索/test.py
+++ ネットワーク内デバイス探索/test.py
@@ -2,37 +2,32 @@ from socket import timeout
 from scapy.all import *
 import subprocess as sbp
 
-# ICMPパケットを送って，返信させてIPアドレスを特定する
-def get_ipaddr(my_ipaddr):
-    #自身のIPアドレスとサブネットマスクからブロードキャストアドレスを求める．
-    count = 0
-    my_ip = []
-    for string in my_ipaddr:
-        if string == '.':
-            count = count + 1
-            my_ip.append(string)
-            if count == 3:
-                break
-        else:
-            my_ip.append(string)
-        
-    print(my_ip)
-
-# ICMPでブロードキャストを流し，返答を受け取ることで各種アドレスを特定
-def broadcastaddr():
+def get_addr(network_part):
     # ICMPパケットを流して結果を1つだけ受け取る dstの後をブロードキャストアドレスにしたいところ
-    packets = Ether(dst='ff:ff:ff:ff:ff:ff')/IP(dst='192.168.1.0/24')/ICMP()
-    answers = srp(packets,timeout=2)
-    print(answers)
-
-# arp
-def get_macaddr():
-    packet = ARP()
-
-
-# hostname
-def get_hostname():
-    packet = ip()
+    # ネットワーク部だけ入力してもらって，後は繰り返す？ 192.168.1.0~192.168.1.254みたいに．
+    #broadcast_ipaddr = 
+    receives_ICMP = []
+    receives_ARP = []
+    ip_addr_list = []
+    for i in range (0,9):
+        i = str(i)
+        dst_addr = network_part + i
+        ip_addr_list.append(dst_addr)
+        
+        print('現在探索中のアドレスは{}'.format(dst_addr))
+        
+        frame = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(op=1, pdst = dst_addr)
+        receive = srp1(frame,timeout=3,iface='en0')
+        try:
+            receives_ARP.append(receive[Ether].src)
+        except:
+            receives_ARP.append(receive)
+        
+        
+        
+    print(ip_addr_list)
+    print(receives_ARP)
+    print(receives_ICMP)
+    #return results
     
-if __name__ == '__main__':
-    broadcastaddr()
+get_addr('192.168.10.')
